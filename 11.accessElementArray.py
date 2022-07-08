2c45442e1cee43cc981600a2f4ffe508cadc1b7a
nikhilgupta11 <guptanikhil1102@gmail.com>
2022-07-08 18:21:23 +0530

    fifth

---
+++ 11.accessElementArray.py
@@ -0,0 +1,20 @@
+from array import *
+from os import access
+
+arr1 = array('d', [1.2, 4, 3, 45, 4, 3])
+
+print(arr1)
+
+
+def accessElement(array, index):
+    if index >= len(array):
+        print("There is no element at this location")
+    else:
+        print(array[index])
+
+
+accessElement(arr1, 5)
+accessElement(arr1, 6)
+
+# here length of array is 6 so if we defien the index 6 then it willl generate an exception
+# to spolve this proble we have to use equal to sign
